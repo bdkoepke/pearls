20c1eafc3c33cee462481d0f0f0c1b8144c8df62
Brandon Koepke <bdkoepke@gmail.com>
2015-06-22 21:53:19 -0700

    Up to the end of chapter 1.

--- HSpecTests.hs
+++ HSpecTests.hs
@@ -4,7 +4,14 @@ import MinFree
 import Test.Hspec
 
 main :: IO ()
-main = hspec $
-	describe "Validate minfree function" $
-		it "minfree should find the smallest number not in the supplied set" $
-			minfree [08, 23, 09, 00, 12, 11, 01, 10, 31, 07, 41, 04, 14, 21, 05, 17, 03, 19, 02, 06] `shouldBe` 15
+main = hspec minfreeTest
+
+minfreeTest :: Spec
+minfreeTest = do
+  describe "Validate minfree function" $
+    it "minfree should find the smallest number not in the supplied set" $
+      minfree [08, 23, 09, 00, 12, 11, 01, 10, 31, 07, 41, 04, 14, 21, 05, 17, 03, 19, 02, 06] `shouldBe` 13
+
+  describe "Validate minfree' function" $
+    it "minfree' should find the smallest number not in the supplied set" $
+      minfree' [08, 23, 09, 00, 12, 11, 01, 10, 31, 07, 41, 04, 14, 21, 05, 17, 03, 19, 02, 06] `shouldBe` 13
--- src/ch01/MinFree.hs
+++ src/ch01/MinFree.hs
@@ -1,13 +1,38 @@
 module MinFree where
-minfree :: [Integer] -> Integer
-minfree xs = head ([0..] \\ xs)
+import Data.Array
+import Data.Array.ST
+import Data.List
 
-(\\) :: Eq a => [a] -> [a] -> [a]
-us \\ vs = filter (notElem' vs) us
+minfree :: [Int] -> Int
+minfree xs = head ([0..] \\ xs)
 
 notElem' :: Eq a => [a] -> a -> Bool
 notElem' a n = n `notElem` a
 
-test =
-  let a = [08, 23, 09, 00, 12, 11, 01, 10, 13, 07, 41, 04, 14, 21, 05, 17, 03, 19, 02, 06]
-  in minfree a
+search :: Array Integer Bool -> Int
+search = length . takeWhile id . elems
+
+checklist xs = accumArray (||) False (0, n) (zip (filter (<= n) xs) (repeat True))
+                 where n = length xs
+
+countlist :: [Int] -> Array Int Int
+countlist xs = accumArray (+) 0 (0, n) (zip xs (repeat 1))
+                 where n = length xs
+
+bucketsort :: [Int] -> [Int]
+bucketsort xs = concat [replicate k x | (x, k) <- assocs (countlist xs)]
+
+checklist' xs = runSTArray (do
+                 {a <- newArray (0, n) False;
+                  sequence_ [writeArray a x True | x <- xs, x <= n];
+                  return a})
+               where n = length xs
+
+minfree' :: [Int] -> Int
+minfree' xs = minfrom 0 (length xs, xs)
+minfrom a (n, xs) | n == 0             = a
+                  | m == b - a         = minfrom b (n - m, vs)
+                  | otherwise          = minfrom a (m, us)
+                   where (us, vs)      = partition (< b) xs
+                         b             = a + 1 + n `div` 2
+                         m             = length us
